f582624f3a19c52801a5c9ef367e74b942b01639
Marcin Wawszczak <marcin.pawel.wawszczak@gmail.com>
2022-06-08 01:15:34 +0200

    Data_Original Plots

--- experiments.py
+++ experiments.py
@@ -1,4 +1,18 @@
-batch_sizes = [8, 16, 32, 64, 128, 256, 512]
+from train import train_model
+
+data_names = ['Data_Original', 'Data_Augmented_3k', 'Data_Augmented_30k', 'Data_Augmented_30k'
+              'HSIL_Original', 'HSIL_1k', 'HSIL_10k', 'HSIL_30k', 'LSIL_Original', 
+              'LSIL_1k', 'LSIL_10k', 'LSIL_30k', 'NSIL_Original', 'NSIL_1k', 'NSIL_10k',
+              'NSIL_30k']
+
+batch_sizes = [8, 16, 32, 64, 128, 256]
 lrs = [2*1e-4, 1e4, 2*1e3, 1e3]
 epoch_snapshot = [1, 5, 10, 20, 30, 40, 50, 70, 80, 90, 100]
+
+# batch_size experiment
+for batch in batch_sizes:
+    train_model(batch_size=batch, data_name=data_names[1])
+
+# learing rate experiment
+
 
--- train.py
+++ train.py
@@ -14,221 +14,185 @@ import tqdm as tqdm
 
 from dcgan import Generator, Discriminator, weights_init
 
-manualSeed = 28180
+def train_model(manualSeed=28180, data_name='Data_PNG', batch_size=128,
+                num_epochs=100, lr=2*1e-4):
 
-print(f"Random seed: {manualSeed}")
-random.seed(manualSeed)
-torch.manual_seed(manualSeed)
+    print(f"Random seed: {manualSeed}")
+    random.seed(manualSeed)
+    torch.manual_seed(manualSeed)
 
-# Root directory for dataset
-data_name = "Data_PNG"
-# dataroot = f"Data/Data_Single/{data_name}"
-dataroot = f"Data/{data_name}"
+    # Path to the data folder
+    dataroot = f"Data/{data_name}"
 
-# Number of workers for dataloader
-workers = 6
-# Batch size during training
-batch_size = 16
-# Spatial size of training images. All images will be resized to this
-#   size using a transformer.
-image_size = 64
-# Number of training epochs
-num_epochs = 100
+    # Number of workers for dataloader
+    workers = 6
+    
+    # Spatial size of training images. All images will be resized to this
+    #   size using a transformer.
+    image_size = 64
 
-# Learning rate for optimizers
-lr = 2 * 1e-4
-# Beta1 hyperparam for Adam optimizers
-beta1 = 0.5
-# Number of GPUs available. Use 0 for CPU mode.
-ngpu = 1
+    # Beta1 hyperparam for Adam optimizers
+    beta1 = 0.5
+    # Number of GPUs available. Use 0 for CPU mode.
+    ngpu = 1
 
-parameters = {
-    'nc': 3,    # Number of channels in the training images. For color images this is 3
-    'nz': 100,  # Size of z latent vector (i.e. size of generator input)
-    'ngf': 64,  # Size of feature maps in generator
-    'ndf': 64,  # Size of feature maps in discriminator
-}
+    parameters = {
+        'nc': 3,    # Number of channels in the training images. For color images this is 3
+        'nz': 100,  # Size of z latent vector (i.e. size of generator input)
+        'ngf': 64,  # Size of feature maps in generator
+        'ndf': 64,  # Size of feature maps in discriminator
+    }
 
-# We can use an image folder dataset the way we have it setup.
-# Create the dataset
-dataset = dset.ImageFolder(root=dataroot,
-                           transform=transforms.Compose([
-                               transforms.Resize(image_size),
-                               transforms.CenterCrop(image_size),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                           ]))
+    # We can use an image folder dataset the way we have it setup.
+    # Create the dataset
+    dataset = dset.ImageFolder(root=dataroot,
+                               transform=transforms.Compose([
+                                         transforms.Resize(image_size),
+                                         transforms.CenterCrop(image_size),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                ]))
 
-# print(dataset.targets)
-# Create the dataloader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         shuffle=True, num_workers=workers)
+    # Create the dataloader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                             shuffle=True, num_workers=workers)
 
-# Decide which device we want to run on
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-print("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    # Decide which device we want to run on
+    device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    print("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
-# Plot some training images
-real_batch = next(iter(dataloader))
-# plt.figure(figsize=(8,8))
-# plt.axis("off")
-# plt.title("Training Images")
-# plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-# plt.show()
+    # Plot some training images
+    real_batch = next(iter(dataloader))
 
-# Create the generator
-netG = Generator(parameters).to(device)
+    # Create the generator
+    netG = Generator(parameters).to(device)
+
+    # Apply the weights_init function to randomly initialize all weights
+    #  to mean=0, stdev=0.02.
+    netG.apply(weights_init)
 
 
-# Apply the weights_init function to randomly initialize all weights
-#  to mean=0, stdev=0.02.
-netG.apply(weights_init)
-
-# Print the model
-# print(netG)
+    # Create the Discriminator
+    netD = Discriminator(parameters).to(device)
 
 
-# Create the Discriminator
-netD = Discriminator(parameters).to(device)
+    # Apply the weights_init function to randomly initialize all weights
+    #  to mean=0, stdev=0.2.
+    netD.apply(weights_init)
 
+    # Initialize BCELoss function
+    criterion = nn.BCELoss()
 
-# Apply the weights_init function to randomly initialize all weights
-#  to mean=0, stdev=0.2.
-netD.apply(weights_init)
+    # Create batch of latent vectors that we will use to visualize
+    #  the progression of the generator
+    fixed_noise = torch.randn(64, parameters['nz'], 1, 1, device=device)
 
-# Print the model
-# print(netD)
+    # Establish convention for real and fake labels during training
+    real_label = 1.
+    fake_label = 0.
 
-# Initialize BCELoss function
-criterion = nn.BCELoss()
+    # Setup Adam optimizers for both G and D
+    optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+    optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
 
-# Create batch of latent vectors that we will use to visualize
-#  the progression of the generator
-fixed_noise = torch.randn(64, parameters['nz'], 1, 1, device=device)
+    # Training Loop
 
-# Establish convention for real and fake labels during training
-real_label = 1.
-fake_label = 0.
+    # Lists to keep track of progress
+    img_list = []
+    G_losses = []
+    D_losses = []
+    iters = 0
 
-# Setup Adam optimizers for both G and D
-optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+    # Chosen epochs to save the model
 
-# Training Loop
+    print("Starting Training Loop...")
+    # For each epoch
+    for epoch in tqdm.tqdm(range(num_epochs)):
+        # For each batch in the dataloader
+        for i, data in enumerate(dataloader, 0):
+            ############################
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+            ###########################
+            ## Train with all-real batch
+            netD.zero_grad()
+            # Format batch
+            real_cpu = data[0].to(device)
+            b_size = real_cpu.size(0)
+            label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+            # Forward pass real batch through D
+            output = netD(real_cpu).view(-1)
+            # Calculate loss on all-real batch
+            errD_real = criterion(output, label)
+            # Calculate gradients for D in backward pass
+            errD_real.backward()
+            D_x = output.mean().item()
 
-# Lists to keep track of progress
-img_list = []
-G_losses = []
-D_losses = []
-iters = 0
+            ## Train with all-fake batch
+            # Generate batch of latent vectors
+            noise = torch.randn(b_size, parameters['nz'], 1, 1, device=device)
+            # Generate fake image batch with G
+            fake = netG(noise)
+            label.fill_(fake_label)
+            # Classify all fake batch with D
+            output = netD(fake.detach()).view(-1)
+            # Calculate D's loss on the all-fake batch
+            errD_fake = criterion(output, label)
+            # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+            errD_fake.backward()
+            D_G_z1 = output.mean().item()
+            # Compute error of D as sum over the fake and the real batches
+            errD = errD_real + errD_fake
+            # Update D
+            optimizerD.step()
 
-# Chosen epochs to save the model
+            ############################
+            # (2) Update G network: maximize log(D(G(z)))
+            ###########################
+            netG.zero_grad()
+            label.fill_(real_label)  # fake labels are real for generator cost
+            # Since we just updated D, perform another forward pass of all-fake batch through D
+            output = netD(fake).view(-1)
+            # Calculate G's loss based on this output
+            errG = criterion(output, label)
+            # Calculate gradients for G
+            errG.backward()
+            D_G_z2 = output.mean().item()
+            # Update G
+            optimizerG.step()
 
-print("Starting Training Loop...")
-# For each epoch
-for epoch in tqdm.tqdm(range(num_epochs)):
-    # For each batch in the dataloader
-    for i, data in enumerate(dataloader, 0):
+            # Output training stats
+            if i % 50 == 0:
+                print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                    % (epoch, num_epochs, i, len(dataloader),
+                        errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
-        ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-        ###########################
-        ## Train with all-real batch
-        netD.zero_grad()
-        # Format batch
-        real_cpu = data[0].to(device)
-        b_size = real_cpu.size(0)
-        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
-        # Forward pass real batch through D
-        output = netD(real_cpu).view(-1)
-        # Calculate loss on all-real batch
-        errD_real = criterion(output, label)
-        # Calculate gradients for D in backward pass
-        errD_real.backward()
-        D_x = output.mean().item()
+            # Save Losses for plotting later
+            G_losses.append(errG.item())
+            D_losses.append(errD.item())
 
-        ## Train with all-fake batch
-        # Generate batch of latent vectors
-        noise = torch.randn(b_size, parameters['nz'], 1, 1, device=device)
-        # Generate fake image batch with G
-        fake = netG(noise)
-        label.fill_(fake_label)
-        # Classify all fake batch with D
-        output = netD(fake.detach()).view(-1)
-        # Calculate D's loss on the all-fake batch
-        errD_fake = criterion(output, label)
-        # Calculate the gradients for this batch, accumulated (summed) with previous gradients
-        errD_fake.backward()
-        D_G_z1 = output.mean().item()
-        # Compute error of D as sum over the fake and the real batches
-        errD = errD_real + errD_fake
-        # Update D
-        optimizerD.step()
+            # Check how the generator is doing by saving G's output on fixed_noise
+            if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
+                with torch.no_grad():
+                    fake = netG(fixed_noise).detach().cpu()
+                img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
-        ############################
-        # (2) Update G network: maximize log(D(G(z)))
-        ###########################
-        netG.zero_grad()
-        label.fill_(real_label)  # fake labels are real for generator cost
-        # Since we just updated D, perform another forward pass of all-fake batch through D
-        output = netD(fake).view(-1)
-        # Calculate G's loss based on this output
-        errG = criterion(output, label)
-        # Calculate gradients for G
-        errG.backward()
-        D_G_z2 = output.mean().item()
-        # Update G
-        optimizerG.step()
+            iters += 1
 
-        # Output training stats
-        if i % 25 == 0:
-            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                  % (epoch, num_epochs, i, len(dataloader),
-                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+        # Save model at specified epoch.
+        epoch_snapshot = [1, 5, 10, 20, 30, 40, 50, 70, 80, 90, 100]
+        for snap in epoch_snapshot:
+            if epoch + 1 == snap:
+                isExist = os.path.exists(f'Models/{data_name}')
+                if not isExist:
+                    os.makedirs(f'Models/{data_name}')
 
-        # Save Losses for plotting later
-        G_losses.append(errG.item())
-        D_losses.append(errD.item())
-
-        # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
-            with torch.no_grad():
-                fake = netG(fixed_noise).detach().cpu()
-            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
-
-        iters += 1
-
-    # Save model at specified epoch.
-    epoch_snapshot = [1, 5, 10, 20, 30, 40, 50, 70, 80, 90, 100]
-    for snap in epoch_snapshot:
-        if epoch + 1 == snap:
-            isExist = os.path.exists(f'Models/{data_name}')
-            if not isExist:
-                os.makedirs(f'Models/{data_name}')
-
-            torch.save({
-                        'generator' : netG.state_dict(),
-                        'discriminator' : netD.state_dict(),
-                        'optimizerG' : optimizerG.state_dict(),
-                        'optimizerD' : optimizerD.state_dict(),
-                        'GLosses': G_losses,
-                        'DLosses': D_losses,
-                        'params' : parameters
-                        }, f'Models/{data_name}/e{snap}.pth')
-            print(f'Epoch {snap} saved...')
-
-
-# Grab a batch of real images from the dataloader
-real_batch = next(iter(dataloader))
-
-# Plot the real images
-
-save_path = f'Plots&Images/{data_name}'
-isExist = os.path.exists(save_path)
-if not isExist:
-    os.makedirs(save_path)
-plt.figure(figsize=(15,15))
-plt.axis("off")
-plt.title("Real Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
-plt.savefig(f'{save_path}/Original_Images.png')
+                torch.save({
+                            'generator' : netG.state_dict(),
+                            'discriminator' : netD.state_dict(),
+                            'optimizerG' : optimizerG.state_dict(),
+                            'optimizerD' : optimizerD.state_dict(),
+                            'GLosses': G_losses,
+                            'DLosses': D_losses,
+                            'params' : parameters
+                            }, f'Models/{data_name}/b{batch_size}_e{snap}.pth')
+                print(f'Epoch {snap} saved...')
